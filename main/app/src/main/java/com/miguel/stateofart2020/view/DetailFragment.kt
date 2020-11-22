97d929ef57ddcea2c95b5ea3da9bc996d399cd44
Miguel <miguelmartins018@gmail.com>
2020-11-22 21:09:36 +0000

    play with navigation controller

---
+++ main/app/src/main/java/com/miguel/stateofart2020/view/DetailFragment.kt
@@ -0,0 +1,31 @@
+package com.miguel.stateofart2020.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+
+import com.miguel.stateofart2020.R
+import kotlinx.android.synthetic.main.fragment_detail.*
+
+
+class DetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        buttonList.setOnClickListener {
+            Navigation.findNavController(it).navigate(DetailFragmentDirections.actionBackToDetail())
+        }
+
+    }
+
+}
---
+++ main/app/src/main/java/com/miguel/stateofart2020/view/ListFragment.kt
@@ -0,0 +1,27 @@
+package com.miguel.stateofart2020.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+
+import com.miguel.stateofart2020.R
+import kotlinx.android.synthetic.main.fragment_list.*
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonDetail.setOnClickListener {
+            Navigation.findNavController(it).navigate(ListFragmentDirections.actionGoToDetail())
+        }
+    }
+
+}
